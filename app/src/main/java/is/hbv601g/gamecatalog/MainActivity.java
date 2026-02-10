23ccc2dee915f289ef1599ac1b4ef846c1578abd
Jon-Emil <jer8@hi.is>
2026-02-10 14:18:52 +0000

    initial game and network services implemented

--- app/src/main/java/is/hbv601g/gamecatalog/MainActivity.java
+++ app/src/main/java/is/hbv601g/gamecatalog/MainActivity.java
@@ -1,6 +1,7 @@
 package is.hbv601g.gamecatalog;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import is.hbv601g.gamecatalog.services.GameService;
+import is.hbv601g.gamecatalog.services.NetworkService;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GameService gameService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        NetworkService networkService = new NetworkService();
+        gameService = new GameService(networkService);
+
+        gameService.getAllGames(1, "title", false, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(() ->
+                        Log.e("API", "Request failed", e)
+                );
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseBody = response.body().string();
+
+                runOnUiThread(() ->
+                        Log.d("API", responseBody)
+                );
+            }
         });
     }
 }
---
+++ app/src/main/java/is/hbv601g/gamecatalog/services/GameService.java
@@ -0,0 +1,27 @@
+package is.hbv601g.gamecatalog.services;
+
+import okhttp3.Callback;
+
+public class GameService {
+
+    private final NetworkService networkService;
+
+    public GameService(NetworkService networkService) {
+        this.networkService = networkService;
+    }
+
+    public void getSpecificGame(long gameID, Callback callback) {
+        String idString = String.valueOf(gameID);
+        String url = "/games/" + idString;
+
+        networkService.getRequest(url, callback);
+    }
+
+    public void getAllGames(int page , String sortBy, boolean sortReverse, Callback callback) {
+        String pageString = String.valueOf(page);
+        String reverseString = sortReverse ? "true" : "false";
+        String url = "/games?pageNr=" + pageString + "&sortBy=" + sortBy + "&sortReverse=" + reverseString;
+
+        networkService.getRequest(url, callback);
+    }
+}
