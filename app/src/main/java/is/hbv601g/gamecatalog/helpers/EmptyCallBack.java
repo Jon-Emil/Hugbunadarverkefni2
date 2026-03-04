0b0fcee0d69bc86ed3e2d170410227f4a2bfb19f
Jon-Emil <jer8@hi.is>
2026-03-04 11:24:06 +0000

    initial implementation

---
+++ app/src/main/java/is/hbv601g/gamecatalog/helpers/EmptyCallBack.java
@@ -0,0 +1,6 @@
+package is.hbv601g.gamecatalog.helpers;
+
+public interface EmptyCallBack {
+    void onSuccess();
+    void onError(Exception e);
+}
--- app/src/main/java/is/hbv601g/gamecatalog/pages/specific_game/SpecificGameFragment.java
+++ app/src/main/java/is/hbv601g/gamecatalog/pages/specific_game/SpecificGameFragment.java
@@ -28,8 +28,10 @@ import is.hbv601g.gamecatalog.adapters.ReviewAdapter;
 import is.hbv601g.gamecatalog.databinding.FragmentAllGamesBinding;
 import is.hbv601g.gamecatalog.databinding.FragmentSpecificGameBinding;
 import is.hbv601g.gamecatalog.entities.game.DetailedGameEntity;
+import is.hbv601g.gamecatalog.helpers.GameCollections;
 import is.hbv601g.gamecatalog.services.GameService;
 import is.hbv601g.gamecatalog.services.NetworkService;
+import is.hbv601g.gamecatalog.services.UserService;
 
 public class SpecificGameFragment extends Fragment {
 
@@ -99,19 +101,77 @@ public class SpecificGameFragment extends Fragment {
         }
         long gameId = args.getLong(ARG_GAME_ID);
 
-
-
         NetworkService networkService = new NetworkService(requireContext());
         GameService gameService = new GameService(networkService);
+        UserService userService = new UserService(networkService);
 
         viewModel = new ViewModelProvider(this).get(SpecificGameViewModel.class);
 
-        viewModel.init(gameService, gameId);
-        // IDE warning auto fix no idea how this::<methodName> works
+        viewModel.init(gameService, userService, gameId);
         viewModel.getGame().observe(getViewLifecycleOwner(), this::updateGameInfo);
+
+        viewModel.getIsProcessingFavorites().observe(getViewLifecycleOwner(), isLoading -> {
+            binding.favoriteButton.setEnabled(!isLoading);
+            if (isLoading) {
+                binding.favoriteButton.setText("Loading...");
+            }
+        });
+        viewModel.getIsInFavorites().observe(getViewLifecycleOwner(), isFavorite -> {
+            binding.favoriteButton.setText(getNewCollectionText(GameCollections.FAVORITE, isFavorite));
+            // couple of unnecessary calls but it's a lightweight function so not a big deal.
+            updateCollectionsAmount();
+        });
+        binding.favoriteButton.setOnClickListener(v -> {
+            Boolean isFavorite = viewModel.getIsInFavorites().getValue();
+            if (isFavorite != null ? isFavorite : false) {
+                viewModel.removeFromCollection(GameCollections.FAVORITE);
+            } else {
+                viewModel.addToCollection(GameCollections.FAVORITE);
+            }
+        });
+
+        viewModel.getIsProcessingWantToPlay().observe(getViewLifecycleOwner(), isLoading -> {
+            binding.wantToPlayButton.setEnabled(!isLoading);
+            if (isLoading) {
+                binding.wantToPlayButton.setText("Loading...");
+            }
+        });
+        viewModel.getIsInWantToPlay().observe(getViewLifecycleOwner(), isInWantToPlay -> {
+            binding.wantToPlayButton.setText(getNewCollectionText(GameCollections.WANT_TO_PLAY, isInWantToPlay));
+            // couple of unnecessary calls but it's a lightweight function so not a big deal.
+            updateCollectionsAmount();
+        });
+        binding.wantToPlayButton.setOnClickListener(v -> {
+            Boolean isInWantToPlay = viewModel.getIsInWantToPlay().getValue();
+            if (isInWantToPlay != null ? isInWantToPlay : false) {
+                viewModel.removeFromCollection(GameCollections.WANT_TO_PLAY);
+            } else {
+                viewModel.addToCollection(GameCollections.WANT_TO_PLAY);
+            }
+        });
+
+        viewModel.getIsProcessingHasPlayed().observe(getViewLifecycleOwner(), isLoading -> {
+            binding.havePlayedButton.setEnabled(!isLoading);
+            if (isLoading) {
+                binding.havePlayedButton.setText("Loading...");
+            }
+        });
+        viewModel.getIsInHasPlayed().observe(getViewLifecycleOwner(), isInHasPlayed -> {
+            binding.havePlayedButton.setText(getNewCollectionText(GameCollections.HAS_PLAYED, isInHasPlayed));
+            // couple of unnecessary calls but it's a lightweight function so not a big deal.
+            updateCollectionsAmount();
+        });
+        binding.havePlayedButton.setOnClickListener(v -> {
+            Boolean isInHasPlayed = viewModel.getIsInHasPlayed().getValue();
+            if (isInHasPlayed != null ? isInHasPlayed : false) {
+                viewModel.removeFromCollection(GameCollections.HAS_PLAYED);
+            } else {
+                viewModel.addToCollection(GameCollections.HAS_PLAYED);
+            }
+        });
     }
 
-    public void updateGameInfo(DetailedGameEntity game) {
+    private void updateGameInfo(DetailedGameEntity game) {
         String title = game.getTitle();
         binding.gameTitle.setText(title);
         binding.gameDescription.setText(game.getDescription());
@@ -145,6 +205,31 @@ public class SpecificGameFragment extends Fragment {
 
         genreAdapter.setData(game.getGenres());
         reviewAdapter.setData(game.getReviews());
+    }
+
+    private String getNewCollectionText(GameCollections selectedCollection, boolean isInCollection) {
+        String newCollectionText = "";
+        switch (selectedCollection) {
+            case FAVORITE:
+                newCollectionText = isInCollection ? "Unfavorite" : "Favorite";
+                break;
+            case WANT_TO_PLAY:
+                newCollectionText = isInCollection ? "Remove From WantToPlay" : "Add to WantToPlay";
+                break;
+            case HAS_PLAYED:
+                newCollectionText = isInCollection ? "Remove From HavePlayed" : "Add to HavePlayed";
+                break;
+        }
+        return newCollectionText;
+    }
+
+    private void updateCollectionsAmount() {
+        DetailedGameEntity game = viewModel.getGame().getValue();
+        if (game == null) { return; }
+
+        binding.gameFavoriteAmount.setText(String.valueOf(game.getFavoriteOf().size()));
+        binding.gameWantToPlayAmount.setText(String.valueOf(game.getWantToPlay().size()));
+        binding.gameHavePlayedAmount.setText(String.valueOf(game.getHavePlayed().size()));
     }
 
     //Code from developer.android.com
